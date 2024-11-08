569755cafb950b32e52c8da21d38566744dde9b6
BrendoAlmeida <brendolee2004@outlook.com>
2024-11-08 09:45:53 -0300

    Hello world

---
+++ build/CMakeFiles/cmakeTeste.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmakeTeste.dir/main.cpp.o"
+  "CMakeFiles/cmakeTeste.dir/main.cpp.o.d"
+  "cmakeTeste"
+  "cmakeTeste.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmakeTeste.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cmakeTeste.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cmakeTeste.
+# This may be replaced when dependencies are built.
